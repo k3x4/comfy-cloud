ea01971b9f6eb4c689b128a7ddbf945584e516dc
k3x4 <xkexagias@gmail.com>
2025-09-14 21:51:29 +0300

    final

---
+++ 04_enable_service.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+USERNAME="${1:-user}"
+SRC="./services/comfy.service"
+DEST="/etc/systemd/system/comfy.service"
+
+if sudo grep -qE '^[[:space:]]*User=' "$DEST"; then
+  sudo sed -i -E "s/^[[:space:]]*User=.*/User=${USERNAME}/" "$DEST"
+else
+  sudo sed -i "/^\[Service\]/a User=${USERNAME}" "$DEST"
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now comfy.service
