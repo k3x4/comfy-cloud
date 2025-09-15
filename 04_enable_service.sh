835a35f71b194eefc027546b1a66ac50731e85f5
k3x4 <xkexagias@gmail.com>
2025-09-15 11:21:33 +0300

    upd fix 04

--- 04_enable_service.sh
+++ 04_enable_service.sh
@@ -5,6 +5,8 @@ USERNAME="${1:-user}"
 SRC="./services/comfy.service"
 DEST="/etc/systemd/system/comfy.service"
 
+sudo cp "$SRC" "$DEST"
+
 if sudo grep -qE '^[[:space:]]*User=' "$DEST"; then
   sudo sed -i -E "s/^[[:space:]]*User=.*/User=${USERNAME}/" "$DEST"
 else
