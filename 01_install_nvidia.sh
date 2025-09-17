a9178d36395d706bc9a5aa3d4806b7796ee8f76e
k3x4 <xkexagias@gmail.com>
2025-09-17 14:09:18 +0300

    upd zram - nodes

--- 01_install_nvidia.sh
+++ 01_install_nvidia.sh
@@ -3,8 +3,13 @@ set -e
 
 sudo apt update && sudo apt -y upgrade
 sudo apt install -y ubuntu-drivers-common build-essential dkms linux-headers-$(uname -r) \
-                    git curl python3-venv python3-dev
+                    git curl python3-venv python3-dev zram-tools
 
 sudo ubuntu-drivers autoinstall || true
+
+echo -e "ALGO=lz4\nPERCENT=25" | sudo tee /etc/default/zramswap
+sudo systemctl enable --now zramswap
+sleep 1
+sudo systemctl is-enabled zramswap
 
 echo "✅ Nvidia driver OK | REBOOT NEEDED"
