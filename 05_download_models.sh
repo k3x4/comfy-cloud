1d01ee0220d042ce05f63645402bec8a05611c86
k3x4 <xkexagias@gmail.com>
2025-09-19 14:22:07 +0300

    sd

--- 05_download_models.sh
+++ 05_download_models.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
-# Κατεβάζει αρχεία από models.txt στον φάκελο models/<subfolder>/ με προαιρετική μετονομασία.
 
 INPUT_FILE="${1:-models.txt}"
+MODELS_DIR="${MODELS_DIR:-$HOME/comfy/models}"
 
 while read -r url subfolder filename; do
-  # Αγνόησε άδειες γραμμές ή σχόλια
   [ -z "$url" ] && continue
   case "$url" in \#*) continue;; esac
 
-  dest_dir="models/${subfolder}"
+  dest_dir="${MODELS_DIR}/${subfolder}"
   mkdir -p "$dest_dir"
 
   if [ -n "$filename" ]; then
-    # Με συγκεκριμένο όνομα αρχείου
     wget -c --content-disposition -O "${dest_dir}/${filename}" "$url"
   else
-    # Χωρίς όνομα -> ό,τι δώσει ο server ή το basename του URL
     wget -c --content-disposition -P "$dest_dir" "$url"
   fi
 done < "$INPUT_FILE"
