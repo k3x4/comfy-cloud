7c0667a8be23c4d073be8d5047b8e1e7f5e3b098
k3x4 <xkexagias@gmail.com>
2025-09-21 20:13:30 +0300

    sd

--- 02_2_install_all.sh
+++ 02_2_install_all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 bash ./02_install_zram.sh
+sleep 3
 bash ./03_install_comfy.sh
 bash ./04_install_custom_nodes.sh
+sleep 3
 bash ./05_download_models.sh
 bash ./06_restore_from_r2.sh
 bash ./07_enable_service.sh
