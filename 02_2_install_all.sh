ce62022b657009a6757199933d5d239bcda9f2f3
k3x4 <xkexagias@gmail.com>
2025-09-21 21:18:46 +0300

    asd

--- 02_2_install_all.sh
+++ 02_2_install_all.sh
@@ -4,7 +4,7 @@ bash ./02_install_zram.sh
 sleep 3
 bash ./03_install_comfy.sh
 bash ./04_install_custom_nodes.sh
-sleep 3
-bash ./05_download_models.sh
-bash ./06_restore_from_r2.sh
-bash ./07_enable_service.sh
+# sleep 3
+# bash ./05_download_models.sh
+# bash ./06_restore_from_r2.sh
+# bash ./07_enable_service.sh
