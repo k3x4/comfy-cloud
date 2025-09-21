c36f377551f0358b408011d696182121570d6d55
k3x4 <xkexagias@gmail.com>
2025-09-21 15:28:28 +0300

    upd install all

---
+++ 02_2_install_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+bash ./02_install_zram.sh
+bash ./03_install_comfy.sh
+bash ./04_install_custom_nodes.sh
+bash ./05_download_models.sh
+bash ./06_restore_from_r2.sh
+bash ./07_enable_service.sh
